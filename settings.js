d5c4448b347d55a9d963fbb6ac92a88644cb57ac
xtenbot56 <wpbot116@gmail.com>
2025-05-22 11:52:07 +0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'Hussain Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
+  botName: "🅣🅐🅗🅘 🅑🅞🅣ᯓᡣ𐭩",
+  botOwner: 'MR', // HUSSAIN X2
   ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
