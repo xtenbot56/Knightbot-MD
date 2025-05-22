440356854952f2ac52557988605b9e7248023ad1
xtenbot56 <wpbot116@gmail.com>
2025-05-22 11:34:59 +0600

    Update facebook.js

--- commands/facebook.js
+++ commands/facebook.js
@@ -84,7 +84,7 @@ async function facebookCommand(sock, chatId, message) {
         await sock.sendMessage(chatId, {
             video: { url: tempFile },
             mimetype: "video/mp4",
-            caption: "𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗𝗘𝗗 𝗕𝗬 𝗞𝗡𝗜𝗚𝗛𝗧-𝗕𝗢𝗧"
+            caption: "𝗗𝗢𝗪𝗡𝗟𝗢𝗔𝗗𝗘𝗗 𝗕𝗬 🅗🅤🅢🅢🅐🅘🅝-🅑🅞🅣"
         }, { quoted: message });
 
         // Clean up temp file
